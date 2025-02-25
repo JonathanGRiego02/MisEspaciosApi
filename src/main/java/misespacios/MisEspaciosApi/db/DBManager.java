ec06638ca9d6138bbbc3e24ef969784eef8955f5
Jonathan Gutiérrez Riego <jonathangrclases@gmail.com>
2025-02-25 16:57:51 +0100

    feat: Types and User models

--- src/main/java/misespacios/MisEspaciosApi/db/DBManager.java
+++ src/main/java/misespacios/MisEspaciosApi/db/DBManager.java
@@ -1,13 +1,7 @@
 package misespacios.MisEspaciosApi.db;
 
-import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.cloud.FirestoreClient;
 
-import java.io.IOException;
-import java.io.InputStream;
 
 public class DBManager {
 
